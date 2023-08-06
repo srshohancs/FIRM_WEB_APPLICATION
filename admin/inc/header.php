efd37a99af75803b0801114cefcbaf15049bed1d
shohanurrahman1 <shohanurrahmanshohan.cs@gmail.com>
2023-08-07 01:50:40 +0600

    Page validdation

---
+++ admin/inc/header.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en" class="semi-dark">
+
+
+<!-- Mirrored from codervent.com/rukada/demo/vertical/ by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 31 Jan 2023 17:26:15 GMT -->
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!--favicon-->
+	<link rel="icon" href="assets/images/favicon-32x32.png" type="image/png" />
+	<!--plugins-->
+	<link href="assets/plugins/vectormap/jquery-jvectormap-2.0.2.css" rel="stylesheet"/>
+	<link href="assets/plugins/simplebar/css/simplebar.css" rel="stylesheet" />
+	<link href="assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css" rel="stylesheet" />
+	<link href="assets/plugins/metismenu/css/metisMenu.min.css" rel="stylesheet" />
+	<!-- loader-->
+	<link href="assets/css/pace.min.css" rel="stylesheet" />
+	<script src="assets/js/pace.min.js"></script>
+	<!-- Bootstrap CSS -->
+	<link href="assets/css/bootstrap.min.css" rel="stylesheet">
+	<link href="assets/css/bootstrap-extended.css" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&amp;display=swap" rel="stylesheet">
+	<link href="assets/css/app.css" rel="stylesheet">
+	<link href="assets/css/icons.css" rel="stylesheet">
+	<!-- Theme Style CSS -->
+	<link rel="stylesheet" href="assets/css/dark-theme.css" />
+	<link rel="stylesheet" href="assets/css/semi-dark.css" />
+	<link rel="stylesheet" href="assets/css/header-colors.css" />
+	<title>Rukada - Responsive Bootstrap 5 Admin Template</title>
+</head>
+
+<body>
+	<!--wrapper-->
+	<div class="wrapper">
+
+		<!-- ########## START: SIDE BAR ########## -->
+		<?php include "inc/Sidebar.php"; ?>
+		<!-- ########## END: SIDE BAR ########## -->
+
+		<!-- ########## START: SIDE BAR ########## -->
+		<?php include "inc/topbar.php"; ?>
+		<!-- ########## END: SIDE BAR ########## -->
---
+++ admin/inc/sidebar.php
@@ -0,0 +1,37 @@
+	<!--sidebar wrapper -->
+	<div class="sidebar-wrapper" data-simplebar="true">
+		<div class="sidebar-header">
+			<div>
+				<img src="assets/images/logo-icon.png" class="logo-icon" alt="logo icon">
+			</div>
+			<div>
+				<h4 class="logo-text">Rukada</h4>
+			</div>
+			<div class="toggle-icon ms-auto"><i class='bx bx-arrow-to-left'></i>
+			</div>
+		</div>
+		<!--navigation-->
+		<ul class="metismenu" id="menu">
+			<li>
+				<a href="dashboard.php" class="">
+					<div class="parent-icon"><i class='bx bx-home-circle'></i>
+					</div>
+					<div class="menu-title">Dashboard</div>
+				</a>
+			</li>
+			<li class="menu-label">UI Elements</li>
+			<li>
+				<a href="javascript:;" class="has-arrow">
+					<div class="parent-icon"><i class='bx bx-cart'></i>
+					</div>
+					<div class="menu-title">eCommerce</div>
+				</a>
+				<ul>
+					<li> <a href="ecommerce-products.html"><i class="bx bx-right-arrow-alt"></i>Products</a>
+					</li>
+				</ul>
+			</li>
+		</ul>
+		<!--end navigation-->
+	</div>
+	<!--end sidebar wrapper -->
