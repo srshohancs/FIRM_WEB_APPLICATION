de5d36a5d1f96cd11a32f41c9353a1461d506457
shohanurrahman1 <shohanurrahmanshohan.cs@gmail.com>
2023-08-16 03:08:43 +0600

    File splite done

---
+++ inc/header.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html>
+	<head>
+
+		<!-- Basic -->
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">	
+
+		<title>Porto - Responsive HTML5 Template</title>	
+
+		<meta name="keywords" content="HTML5 Template" />
+		<meta name="description" content="Porto - Responsive HTML5 Template">
+		<meta name="author" content="okler.net">
+
+		<!-- Favicon -->
+		<link rel="shortcut icon" href="assets/img/favicon.ico" type="image/x-icon" />
+		<link rel="apple-touch-icon" href="assets/img/apple-touch-icon.png">
+
+		<!-- Mobile Metas -->
+		<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, shrink-to-fit=no">
+
+		<!-- Web Fonts  -->
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800%7CShadows+Into+Light%7CPlayfair+Display:400" rel="stylesheet" type="text/css">
+
+		<!-- Vendor CSS -->
+		<link rel="stylesheet" href="assets/vendor/bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" href="assets/vendor/fontawesome-free/css/all.min.css">
+		<link rel="stylesheet" href="assets/vendor/animate/animate.min.css">
+		<link rel="stylesheet" href="assets/vendor/simple-line-icons/css/simple-line-icons.min.css">
+		<link rel="stylesheet" href="assets/vendor/owl.carousel/assets/owl.carousel.min.css">
+		<link rel="stylesheet" href="assets/vendor/owl.carousel/assets/owl.theme.default.min.css">
+		<link rel="stylesheet" href="assets/vendor/magnific-popup/magnific-popup.min.css">
+
+		<!-- Theme CSS -->
+		<link rel="stylesheet" href="assets/css/theme.css">
+		<link rel="stylesheet" href="assets/css/theme-elements.css">
+		<link rel="stylesheet" href="assets/css/theme-blog.css">
+		<link rel="stylesheet" href="assets/css/theme-shop.css">
+
+		<!-- Current Page CSS -->
+		<link rel="stylesheet" href="assets/vendor/rs-plugin/css/settings.css">
+		<link rel="stylesheet" href="assets/vendor/rs-plugin/css/layers.css">
+		<link rel="stylesheet" href="assets/vendor/rs-plugin/css/navigation.css">
+		<link rel="stylesheet" href="assets/vendor/circle-flip-slideshow/css/component.css">
+		
+		<!-- Demo CSS -->
+
+
+		<!-- Skin CSS -->
+		<link rel="stylesheet" href="assets/css/skins/default.css"> 
+
+		<!-- Theme Custom CSS -->
+		<link rel="stylesheet" href="assets/css/custom.css">
+
+		<!-- Head Libs -->
+		<script src="assets/vendor/modernizr/modernizr.min.js"></script>
+
+	</head>
+
+	<body class="loading-overlay-showing" data-plugin-page-transition data-loading-overlay data-plugin-options="{'hideDelay': 500}">
+		<div class="loading-overlay">
+			<div class="bounce-loader">
+				<div class="bounce1"></div>
+				<div class="bounce2"></div>
+				<div class="bounce3"></div>
+			</div>
+		</div>
+		
+		<div class="body">
+		<header id="header" data-plugin-options="{'stickyEnabled': true, 'stickyEnableOnBoxed': true, 'stickyEnableOnMobile': true, 'stickyStartAt': 45, 'stickySetTop': '-45px', 'stickyChangeLogo': true}">
+				<div class="header-body">
+					<div class="header-container container">
+						<div class="header-row">
+
+							<!-- START: LOGO PART -->
+							<div class="header-column">
+								<div class="header-row">
+									<div class="header-logo">
+										<a href="index.html">
+											<img alt="Porto" width="100" height="48" data-sticky-width="82" data-sticky-height="40" data-sticky-top="25" src="assets/img/logo.png">
+										</a>
+									</div>
+								</div>
+							</div>
+							<!-- END: LOGO PART -->
+
+							<div class="header-column justify-content-end">
+								<!-- START: TOP NAVIGATION PART -->
+								<?php include "inc/top_navigation.php"; ?>
+								<!-- END: TOP NAVIGATION PART -->
+
+								<!-- START: Main NAVIGATION PART -->
+								<?php include "inc/navigation.php"; ?>
+								<!-- END: Main NAVIGATION PART -->
+
+								
+							</div>
+						</div>
+					</div>
+				</div>
+			</header>
---
+++ inc/top_navigation.php
@@ -0,0 +1,35 @@
+<!-- START: TOP NAVIGATION PART -->
+								<div class="header-row pt-3">									
+									<nav class="header-nav-top">
+										<ul class="nav nav-pills">
+											<li class="nav-item nav-item-anim-icon d-none d-md-block">
+												<a class="nav-link pl-0" href="about-us.html"><i class="fas fa-angle-right"></i> About Us</a>
+											</li>
+											<li class="nav-item nav-item-anim-icon d-none d-md-block">
+												<a class="nav-link" href="contact-us.html"><i class="fas fa-angle-right"></i> Contact Us</a>
+											</li>
+											<li class="nav-item nav-item-left-border nav-item-left-border-remove nav-item-left-border-md-show">
+												<span class="ws-nowrap"><i class="fas fa-phone"></i> (123) 456-789</span>
+											</li>
+										</ul>
+									</nav>
+									
+									<div class="header-nav-features">
+										<div class="header-nav-feature header-nav-features-search d-inline-flex">
+											<a href="#" class="header-nav-features-toggle" data-focus="headerSearch"><i class="fas fa-search header-nav-top-icon"></i></a>
+											<div class="header-nav-features-dropdown" id="headerTopSearchDropdown">
+												<form role="search" action="page-search-results.html" method="get">
+													<div class="simple-search input-group">
+														<input class="form-control text-1" id="headerSearch" name="q" type="search" value="" placeholder="Search...">
+														<span class="input-group-append">
+															<button class="btn" type="submit">
+																<i class="fa fa-search header-nav-top-icon"></i>
+															</button>
+														</span>
+													</div>
+												</form>
+											</div>
+										</div>
+									</div>
+								</div>
+								<!-- END: TOP NAVIGATION PART -->
